8eab60a965e3963709ac7001af114c4939348462
Emily Kirk <ekirk@emilys-macbook-air-73.local>
2019-04-03 11:15:42 -0500

    Hello World

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var bandMembersLabel: UILabel!
+    @IBOutlet weak var favoriteBandLabel: UILabel!
+    @IBOutlet weak var faveClass: UILabel!
+    @IBOutlet weak var whatFun: UITextField!
+    @IBOutlet weak var actualDuck: UIImageView!
+    @IBOutlet weak var mysteryButton: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var putNameHere: UITextField!
+    @IBOutlet weak var largerRubberDuck: UIImageView!
+    @IBOutlet weak var UIButton: UIButton!
+    @IBOutlet weak var UILabel: UILabel!
+ 
+    let favoriteBand = "The Beatles"
+    let numberOfMembers = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        faveClass.text = "Mobile Apps!"
+        mysteryButton.backgroundColor = .yellow
+        favoriteBandLabel.text = favoriteBand
+        bandMembersLabel.text = " \(favoriteBand) have \(numberOfMembers) members in the band"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
